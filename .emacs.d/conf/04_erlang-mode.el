ff59aa54b30a8536af7017a9fb8da63ca5cd2280
David.Alpha.Fox <david.alpha.fox@gmail.com>
2013-03-18 14:34:08 +0800

    clean all

--- .emacs.d/conf/04_erlang-mode.el
+++
@@ -1,5 +0,0 @@
-(add-to-list 'load-path "/usr/local/lib/erlang/lib/tools-2.6.10/emacs")
-(require 'erlang-start)
-
-(add-to-list 'auto-mode-alist '("\\.erl?$" . erlang-mode))
-(add-to-list 'auto-mode-alist '("\\.hrl?$" . erlang-mode))
--- .emacs.d/conf/65_auto-complete.el
+++
@@ -1,3 +0,0 @@
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp/ac-dict")
-(ac-config-default)
