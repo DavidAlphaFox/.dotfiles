2abb45a281b03a62eb0caa572e257dc7d1b58813
David.Gao <david.alpha.fox@gmail.com>
2013-03-26 20:51:06 +0800

    change slime

--- .emacs.d/conf/09_lisp-mode.el
+++ .emacs.d/conf/09_lisp-mode.el
@@ -2,7 +2,7 @@
 (require 'slime-autoloads)
 (slime-setup '(slime-fancy))
 (add-to-list 'auto-mode-alist '("\\.lisp?$" . lisp-mode))
-(setq slime-lisp-implementations
-      '((sbcl ("sbcl")
-              :init (lambda (port-file _)
-                      (format "(ql:quickload :swank)\n (swank:start-server %S)\n" port-file)))))
+;;(setq slime-lisp-implementations
+;;      '((sbcl ("sbcl")
+;;              :init (lambda (port-file _)
+;;                      (format "(ql:quickload :swank)\n (swank:start-server %S)\n" port-file)))))
