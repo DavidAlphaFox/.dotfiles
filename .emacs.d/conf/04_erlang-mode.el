57ed2634471ff96b89c34074a9da1abda1e85fea
David.Gao <david.alpha.fox@gmail.com>
2013-03-26 19:24:47 +0800

    make distel work but not pefect

--- .emacs.d/conf/04_erlang-mode.el
+++ .emacs.d/conf/04_erlang-mode.el
@@ -1,6 +1,9 @@
 (add-to-list 'load-path 
              (car (file-expand-wildcards "/usr/local/lib/erlang/lib/tools-*/emacs")))
 
+(setq erlang-root-dir "/usr/local/lib/erlang")
+(setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
+
 (require 'erlang-start)
 
 (add-to-list 'auto-mode-alist '("\\.erl?$" . erlang-mode))
@@ -8,3 +11,19 @@
 
 (require 'distel)
 (distel-setup)
+
+;; prevent annoying hang-on-compile
+(defvar inferior-erlang-prompt-timeout t)
+;;(interactive)
+;; default node name to emacs@localhost
+(setq inferior-erlang-machine-options '("-name" "emacs@127.0.0.1"))
+
+;; tell distel to default to that node
+(setq erl-nodename-cache 'emacs@127.0.0.1)
+     ;; (make-symbol
+      ;; (concat
+       ;; "emacs@"
+        ;; Mac OS X uses "name.local" instead of "name", this should work
+        ;; pretty much anywhere without having to muck with NetInfo
+        ;; ... but I only tested it on Mac OS X.
+       ;; (car (split-string (shell-command-to-string "hostname"))))))
--- .emacs.d/conf/09_d-mode.el
+++
@@ -1,3 +0,0 @@
-(autoload 'd-mode  "d-mode" nil t)
-(require 'd-mode)
-(add-to-list 'auto-mode-alist '("\\.d?$" . d-mode))
