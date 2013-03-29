e3e83f5942506855d0ec794cfcd3fd45f5f1bbc3
David.Gao <david.alpha.fox@gmail.com>
2013-03-29 23:51:34 +0800

    chang rebar config

--- .emacs.d/conf/05_rebar.el
+++ .emacs.d/conf/05_rebar.el
@@ -1,6 +1,17 @@
 (require 'dominating-file)
 (require 'rebar-config)
-(defun ebm-compile-prj ()
-  (interactive)
-  (set (make-local-variable 'compile-command) (format "make -f %sMakefile" (ebm-find-rebar-top)))
+
+(defun rebar-generate-current (project-dir)
+  (interactive (list 
+    (let* ((project-dir (ebm-find-rebar-top))
+           (prompt (if project-dir
+                       (format "Project dir (default %s): " project-dir)
+                     "Project dir: ")))
+           (intern (read-string prompt nil nil project-dir)))))
+  (warn "project dir: %s" project-dir)
+  (rebar-generate-project project-dir))
+  
+(defun rebar-generate-project (project-dir)
+  (set (make-local-variable 'compile-command) (format "make -f %sMakefile" project-dir))
   (call-interactively 'compile))
+
