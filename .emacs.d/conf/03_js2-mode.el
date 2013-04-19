fea31e7476366492d3eecdd15bd176b297e931f1
David.Gao <david.alpha.fox@gmail.com>
2013-04-19 17:36:24 +0800

    use html-mode when edit ejs

--- .emacs.d/conf/03_js2-mode.el
+++ .emacs.d/conf/03_js2-mode.el
@@ -1,6 +1,6 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-
+(add-to-list 'auto-mode-alist '("\\.ejs$" . html-mode))
 (setq js2-cleanup-whitespace t)
 (setq js2-basic-offset 2)
 (setq js2-bounce-indent-p t)
