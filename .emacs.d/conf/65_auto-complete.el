0eb44667159012e55af8ec6b14639e762b0a09dc
David Gao <david@David-iBook.local>
2013-04-25 01:50:11 +0800

    fix bug in auto-compile config

--- .emacs.d/conf/65_auto-complete.el
+++ .emacs.d/conf/65_auto-complete.el
@@ -1,3 +1,3 @@
 (require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp/ac-dict")
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp/auto-complete/ac-dict")
 (ac-config-default)
