c1c4448937fdbb00af8b886586e89f5f65bc565e
David.Gao <david.alpha.fox@gmail.com>
2013-04-25 17:29:31 +0800

    change theme

--- .emacs.d/conf/98_themes.el
+++ .emacs.d/conf/98_themes.el
@@ -1,8 +1,8 @@
 (cond
  ((>= emacs-major-version 24) 
   ;;(load-theme 'solarized-dark t)
-  (load-theme 'zenburn t)
-  ;;(load-theme 'molokai t)
+  ;;(load-theme 'zenburn t)
+  (load-theme 'molokai t)
   )
  (t (load-theme 'molokai)))
 
