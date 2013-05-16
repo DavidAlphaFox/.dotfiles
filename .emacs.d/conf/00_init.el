a638980202b58394dbff6f2ba47f86ab30412829
David.Gao <david.alpha.fox@gmail.com>
2013-05-16 21:36:17 +0800

    add nc tools and improve yank-pop

--- .emacs.d/conf/00_init.el
+++ .emacs.d/conf/00_init.el
@@ -89,3 +89,24 @@
 (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
 
 
+(dolist (command '(yank yank-pop))
+  (eval
+   `(defadvice ,command (after indent-region activate)
+      (and (not current-prefix-arg)
+           (member major-mode
+                   '(emacs-lisp-mode
+                     lisp-mode
+                     clojure-mode
+                     scheme-mode
+                     haskell-mode
+                     ruby-mode
+                     rspec-mode
+                     python-mode
+                     c-mode
+                     c++-mode
+                     objc-mode
+                     latex-mode
+                     js-mode
+                     plain-tex-mode))
+           (let ((mark-even-if-inactive transient-mark-mode))
+             (indent-region (region-beginning) (region-end) nil))))))
