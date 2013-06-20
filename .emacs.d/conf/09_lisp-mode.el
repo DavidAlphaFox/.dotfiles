0f6de37b74e9cb8fcde25d1681a07d3734dfb1b2
David.Gao <david.alpha.fox@gmail.com>
2013-06-20 09:41:02 +0800

    change slime config

--- .emacs.d/conf/09_lisp-mode.el
+++ .emacs.d/conf/09_lisp-mode.el
@@ -3,7 +3,7 @@
 (slime-setup '(slime-fancy))
 (add-to-list 'auto-mode-alist '("\\.lisp$" . lisp-mode))
 (add-to-list 'auto-mode-alist '("\\.asd$" . lisp-mode))
-
+(setq slime-net-coding-system 'utf-8-unix)
 ;;(setq slime-lisp-implementations
 ;;      '((sbcl ("sbcl")
 ;;              :init (lambda (port-file _)
