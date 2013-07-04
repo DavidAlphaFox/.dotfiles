3a24dc91203b684a5e67bb932cb33c3a8e688efb
David.Gao <david.alpha.fox@gmail.com>
2013-07-04 21:04:43 +0800

    delete yasnippet

--- .emacs.d/conf/65_auto-complete.el
+++ .emacs.d/conf/65_auto-complete.el
@@ -1,6 +1,3 @@
-(require 'yasnippet)
-(yas-global-mode 1) 
-
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp/auto-complete/ac-dict")
 (global-auto-complete-mode t)
