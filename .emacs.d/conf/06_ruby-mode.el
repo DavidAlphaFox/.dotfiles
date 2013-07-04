3b27952d2a7f2b449c7bce15e1f9cbe8d89e6227
David.Gao <david.alpha.fox@gmail.com>
2013-07-04 21:21:33 +0800

    fix local key

--- .emacs.d/conf/06_ruby-mode.el
+++ .emacs.d/conf/06_ruby-mode.el
@@ -11,15 +11,6 @@
   (ruby-indent-line t)
   (end-of-line))
 
-;; Local key bindings
-(add-hook 'ruby-mode-hook
-          (lambda ()
-            ;; (ruby-electric-mode)
-            (local-set-key [(control c) (control e)] 'ruby-insert-end)
-            (local-set-key [(control meta f1)] 'xmp) ;; gem install rcodetools
-            (local-set-key [(control meta shift f1)] 'ruby-eval-buffer)
-            (local-set-key (kbd "TAB") 'smart-tab)
-            ))
 
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
