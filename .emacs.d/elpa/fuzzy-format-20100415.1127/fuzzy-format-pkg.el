d60c79778b63c9df6ecd437c48876228c6051e2d
David.Gao <david.alpha.fox@gmail.com>
2013-10-01 04:21:58 +0800

    add fuzzy

---
+++ .emacs.d/elpa/fuzzy-format-20100415.1127/fuzzy-format-pkg.el
@@ -0,0 +1,1 @@
+(define-package "fuzzy-format" "20100415.1127" "select indent-tabs-mode and format code automatically. [wiki]" (quote nil))
---
+++ .emacs.d/elpa/fuzzy-match-20130723.2319/fuzzy-match-pkg.el
@@ -0,0 +1,1 @@
+(define-package "fuzzy-match" "20130723.2319" "fuzzy matching [wiki]" (quote nil))
