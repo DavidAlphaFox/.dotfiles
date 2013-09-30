9a4a3796c54e8a53a3b9cbdfefd75a790e841cf1
David.Gao <david.alpha.fox@gmail.com>
2013-10-01 03:11:25 +0800

    add config file

---
+++ .emacs.d/conf/65_auto-complete.el
@@ -0,0 +1,11 @@
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp/auto-complete/ac-dict")
+(global-auto-complete-mode t)
+(ac-config-default)
+
+(require 'ac-slime)
+(add-hook 'slime-mode-hook 'set-up-slime-ac)
+(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'slime-repl-mode))
+
