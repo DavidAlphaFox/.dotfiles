b05a4d94971377ff13c2fa94b2aa639545557655
David.Gao <david.alpha.fox@gmail.com>
2014-02-10 21:53:48 +0800

    add eply python

---
+++ .emacs.d/elpa/elpy-20131109.1120/elpy-pkg.el
@@ -0,0 +1,10 @@
+(define-package "elpy" "20131109.1120" "Emacs Python Development Environment"
+  '((auto-complete "1.4")
+    (find-file-in-project "3.2")
+    (fuzzy "0.1")
+    (highlight-indentation "0.5.0")
+    (idomenu "0.1")
+    (iedit "0.97")
+    (nose "0.1.1")
+    (virtualenv "1.2")
+    (yasnippet "0.8.0")))
