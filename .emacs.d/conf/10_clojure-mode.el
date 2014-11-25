54f5e7a7a5354870d6d1cbdc5d393bce5ded52a0
David.Gao <david.alpha.fox@gmail.com>
2014-11-26 01:25:58 +0800

    clojure mode

--- .emacs.d/conf/10_clojure-mode.el
+++ .emacs.d/conf/10_clojure-mode.el
@@ -1,23 +1,12 @@
 (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
-;; Enable paredit for Clojure
-(add-hook 'clojure-mode-hook 'enable-paredit-mode)
 
 ;; This is useful for working with camel-case tokens, like names of
 ;; Java classes (e.g. JavaClassName)
 (add-hook 'clojure-mode-hook 'subword-mode)
 
-;; A little more syntax highlighting
-(require 'clojure-mode-extra-font-locking)
 
-;; syntax hilighting for midje
-(add-hook 'clojure-mode-hook
-          (lambda ()
-            (setq inferior-lisp-program "lein repl")
-            (font-lock-add-keywords
-             nil
-             '(("(\\(facts?\\)"
-                (1 font-lock-keyword-face))
-               ("(\\(background?\\)"
-                (1 font-lock-keyword-face))))
-            (define-clojure-indent (fact 1))
-            (define-clojure-indent (facts 1))))
+(eval-after-load 'nrepl
+  '(progn
+     (define-key clojure-mode-map (kbd "C-c M-b") 'nrepl-eval-buffer)
+	 (define-key clojure-mode-map (kbd "C-c M-r") 'nrepl-eval-region)
+	 ))
