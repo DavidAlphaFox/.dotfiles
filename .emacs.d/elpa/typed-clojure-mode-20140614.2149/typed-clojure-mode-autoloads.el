b156955e1716779aab94f70817a58c3782ba7aaf
David.Gao <david.alpha.fox@gmail.com>
2014-11-26 01:05:10 +0800

    add clojure mode

--- .emacs.d/elpa/typed-clojure-mode-20140614.2149/typed-clojure-mode-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; typed-clojure-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (typed-clojure-mode) "typed-clojure-mode" "typed-clojure-mode.el"
-;;;;;;  (21620 43352 0 0))
-;;; Generated autoloads from typed-clojure-mode.el
-
-(autoload 'typed-clojure-mode "typed-clojure-mode" "\
-The official minor mode for editing Typed Clojure. Provides
-namespace typechecking, error navigation, display of type data,
-and annotation snippets.
-
-\\{typed-clojure-mode-map}
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("typed-clojure-mode-pkg.el") (21620 43352
-;;;;;;  884715 0))
-
-;;;***
-
-(provide 'typed-clojure-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; typed-clojure-mode-autoloads.el ends here
