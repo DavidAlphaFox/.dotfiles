ac79cf7bdf47d4f169c8929d8b23269dddff5822
David Gao <david@David-iBook.local>
2014-11-28 13:08:29 +0800

    add ln.sh

---
+++ ln.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+ln -sf /usr/local/bin/ruby20 /usr/local/bin/ruby
+ln -sf /usr/local/bin/erb20 /usr/local/bin/erb
+ln -sf /usr/local/bin/irb20 /usr/local/bin/irb
+ln -sf /usr/local/bin/rdoc20 /usr/local/bin/rdoc
+ln -sf /usr/local/bin/ri20 /usr/local/bin/ri
+ln -sf /usr/local/bin/rake20 /usr/local/bin/rake
+ln -sf /usr/local/bin/gem20 /usr/local/bin/gem
+ln -sf /usr/local/bin/testrb20 /usr/local/bin/testrb
+
+ln -sf /usr/local/bin/python2.7 /usr/local/bin/python
+ln -sf /usr/local/bin/python2.7-2to3 /usr/local/bin/2to3
+ln -sf /usr/local/bin/python2.7-config /usr/local/bin/python-config
+ln -sf /usr/local/bin/pydoc2.7  /usr/local/bin/pydoc
