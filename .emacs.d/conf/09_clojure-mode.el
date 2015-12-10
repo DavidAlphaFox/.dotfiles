20d5f218e4794cbde09a5c2e267b08c08334c295
David.Gao <tianpo.gao@asiainnovations.com>
2015-12-10 12:32:55 +0800

    添加Clojure的支持

---
+++ .emacs.d/conf/09_clojure-mode.el
@@ -0,0 +1,26 @@
+(require 'clojure-mode)
+(setq auto-mode-alist (cons '("\\.cljs$" . clojure-mode) auto-mode-alist))
+(setq inferior-lisp-program "lein repl")
+
+
+(require 'yasnippet)
+(require 'clojure-snippets)
+(yas-global-mode 1)
+(add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets")
+(yas-load-directory "~/.emacs.d/snippets")
+
+
+(global-set-key (kbd "C-c C-s") 'clojure-toggle-keyword-string)
+
+(defun reload-current-clj-ns ()
+  (interactive)
+  (let ((current-point (point)))
+	(goto-char (point-min))
+	(let ((ns-idx (re-search-forward clojure-namespace-name-regex nil t)))
+	  (when ns-idx
+		(goto-char ns-idx)
+		(let ((sym (symbol-at-point)))
+		  (message (format "Loading %s ..." sym))
+		  (lisp-eval-string (format "(require '%s :reload)" sym))
+		  (lisp-eval-string (format "(in-ns '%s)" sym)))))
+	(goto-char current-point)))
