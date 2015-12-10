230a2ed4faa228670a44873b800ba1fc9110b560
David.Gao <tianpo.gao@asiainnovations.com>
2015-12-10 13:34:16 +0800

    cider模式加强

--- .emacs.d/conf/09_clojure-mode.el
+++ .emacs.d/conf/09_clojure-mode.el
@@ -1,6 +1,14 @@
 (require 'clojure-mode)
 (setq auto-mode-alist (cons '("\\.cljs$" . clojure-mode) auto-mode-alist))
 
+(require 'ac-cider)
+(add-hook 'cider-mode-hook 'ac-cider-setup)
+(add-hook 'cider-repl-mode-hook 'ac-cider-setup)
+(eval-after-load "auto-complete"
+  '(progn
+	 (add-to-list 'ac-modes 'cider-mode)
+	 (add-to-list 'ac-modes 'cider-repl-mode)))
+
 
 (require 'yasnippet)
 (require 'clojure-snippets)
@@ -8,18 +16,3 @@
 (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets")
 (yas-load-directory "~/.emacs.d/snippets")
 
-
-(global-set-key (kbd "C-c C-s") 'clojure-toggle-keyword-string)
-
-(defun reload-current-clj-ns ()
-  (interactive)
-  (let ((current-point (point)))
-	(goto-char (point-min))
-	(let ((ns-idx (re-search-forward clojure-namespace-name-regex nil t)))
-	  (when ns-idx
-		(goto-char ns-idx)
-		(let ((sym (symbol-at-point)))
-		  (message (format "Loading %s ..." sym))
-		  (lisp-eval-string (format "(require '%s :reload)" sym))
-		  (lisp-eval-string (format "(in-ns '%s)" sym)))))
-	(goto-char current-point)))
