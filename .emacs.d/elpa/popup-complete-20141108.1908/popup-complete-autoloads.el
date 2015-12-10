ec145364fed261976c7f8c4e55dc018911f3fb68
David.Gao <david.alpha.fox@gmail.com>
2015-12-10 22:51:23 +0800

    删除不必要的组建

--- .emacs.d/elpa/popup-complete-20141108.1908/popup-complete-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; popup-complete-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "popup-complete" "popup-complete.el" (22121
-;;;;;;  35444 0 0))
-;;; Generated autoloads from popup-complete.el
-
-(autoload 'popup-complete--in-region "popup-complete" "\
-
-
-\(fn NEXT-FUNC START END COLLECTION &optional PREDICATE)" nil nil)
-
-(add-hook 'completion-in-region-functions 'popup-complete--in-region)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; popup-complete-autoloads.el ends here
--- .emacs.d/elpa/popup-complete-20141108.1908/popup-complete-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "popup-complete" "20141108.1908" "completion with popup" '((popup "0.5.0")) :url "https://github.com/syohex/emacs-popup-complete")
