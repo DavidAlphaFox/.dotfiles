ed472b525b15d741d5608f24867770890b3db2d1
David.Gao <david.alpha.fox@gmail.com>
2015-12-11 08:24:54 +0800

    添加AC

--- .emacs.d/conf/02_auto-complete.el
+++ .emacs.d/conf/02_auto-complete.el
@@ -1,5 +1,6 @@
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete-*/dict/")
+(auto-complete-mode t)
 (global-auto-complete-mode t)
-(ac-config-default)
+(auto-complete-mode t)
 
---
+++ .emacs.d/elpa/flymake-yaml-20130423.848/flymake-yaml-pkg.el
@@ -0,0 +1,1 @@
+(define-package "flymake-yaml" "20130423.848" "A flymake handler for YAML" '((flymake-easy "0.1")) :url "https://github.com/yasuyk/flymake-yaml" :keywords '("yaml"))
