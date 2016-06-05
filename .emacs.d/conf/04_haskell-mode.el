4a48149f4473d7dd81bf03492d1260e0587d67c1
David.Gao <david.alpha.fox@gmail.com>
2016-06-05 08:18:05 +0800

    material theme

--- .emacs.d/conf/04_haskell-mode.el
+++ .emacs.d/conf/04_haskell-mode.el
@@ -1,2 +1,2 @@
 (require 'haskell-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
--- .emacs.d/conf/98_themes.el
+++ .emacs.d/conf/98_themes.el
@@ -3,6 +3,7 @@
   ;;(load-theme 'solarized-dark t)
   ;;(load-theme 'zenburn t)
   ;;(load-theme 'molokai t)
+  (load-theme 'material t)
   )
  (t
   ;;(load-theme 'molokai)
