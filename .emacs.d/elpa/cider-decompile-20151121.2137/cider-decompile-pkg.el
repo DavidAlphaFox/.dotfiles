aa4a233392da71bb0505a9000c0ed9c9208d3ccf
David.Gao <david.alpha.fox@gmail.com>
2016-11-25 21:28:48 +0800

    upgrade on FreeBSD

--- .emacs.d/elpa/cider-decompile-20151121.2137/cider-decompile-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "cider-decompile" "20151121.2137" "decompilation extension for cider" '((cider "0.3.0") (javap-mode "9")) :url "http://www.github.com/clojure-emacs/cider-decompile" :keywords '("languages" "clojure" "cider"))
