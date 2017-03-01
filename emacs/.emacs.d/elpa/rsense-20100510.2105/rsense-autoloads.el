43262c702a5eac7f928b96fb5e91c8d05d7538fe
David.Gao <david.alpha.fox@gmail.com>
2017-03-01 09:22:26 +0800

    ruby-mode

---
+++ emacs/.emacs.d/elpa/rsense-20100510.2105/rsense-autoloads.el
@@ -0,0 +1,15 @@
+;;; rsense-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("rsense.el") (22710 8732 374609 269000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; rsense-autoloads.el ends here
