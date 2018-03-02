08b035aca345b057d6065a346dacf557cb2c5be8
David.Gao <david.alpha.fox@gmail.com>
2018-03-02 16:44:29 +0800

    add vue support

---
+++ emacs/.emacs.d/elpa/vue-html-mode-20180104.1421/vue-html-mode-autoloads.el
@@ -0,0 +1,27 @@
+;;; vue-html-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "vue-html-mode" "vue-html-mode.el" (23193 3800
+;;;;;;  828365 458000))
+;;; Generated autoloads from vue-html-mode.el
+
+(autoload 'vue-html-mode "vue-html-mode" "\
+Major mode for Vue.js templates.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("vue-html-mode-pkg.el") (23193 3799 80366
+;;;;;;  79000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; vue-html-mode-autoloads.el ends here
