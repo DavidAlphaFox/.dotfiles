b1493e220ca79c5fd3694bba504e7c7dffa5cb30
David.Gao <david.alpha.fox@gmail.com>
2019-07-03 09:25:10 +0800

    update tab config

--- emacs/spacemacs.d/user-config.el
+++ emacs/spacemacs.d/user-config.el
@@ -6,10 +6,6 @@
   (setq default-buffer-file-coding-system 'utf-8)
   (set-default-coding-systems 'utf-8)
   (prefer-coding-system 'utf-8))
-(setq indent-tabs-mode t) 
-(setq default-tab-width 2)
-(setq tab-width 2)
-(setq indent-line-function 'insert-tab)
 
 (progn
   (setq-default fill-column 80)
@@ -22,3 +18,23 @@
 (require 'slime)
 (load-file "~/.spacemacs.d/elisp/slime-repl-ansi-color.el")
 (slime-repl-ansi-on)
+
+(setq indent-tabs-mode nil)
+(setq default-tab-width 2)
+(setq tab-width 2)
+(setq indent-line-function 'insert-tab)
+
+(defun my-setup-indent (n)
+  ;; java/c/c++
+  (setq c-basic-offset n)
+  ;; web development
+  (setq coffee-tab-width n) ; coffeescript
+  (setq javascript-indent-level n) ; javascript-mode
+  (setq js-indent-level n) ; js-mode
+  (setq js2-basic-offset n) ; js2-mode, in latest js2-mode, it's alias of js-indent-level
+  (setq web-mode-markup-indent-offset n) ; web-mode, html tag in html file
+  (setq web-mode-css-indent-offset n) ; web-mode, css in html file
+  (setq web-mode-code-indent-offset n) ; web-mode, js code in html file
+  (setq css-indent-offset n) ; css-mode
+  )
+(my-setup-indent 2)
