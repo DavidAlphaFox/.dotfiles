9b9cdd31c7c9a8e2ccfdd6dfeb12ad4f79377203
David.Gao <david.alpha.fox@gmail.com>
2019-08-06 02:21:48 +0800

    update emacs config

--- emacs/doom.d/config.el
+++ emacs/doom.d/config.el
@@ -1,8 +1,4 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
-(when (file-exists-p "/usr/local/bin/sbcl")
-  (setq inferior-lisp-program "/usr/local/bin/sbcl"))
-(when (file-exists-p "/usr/bin/sbcl")
-  (setq inferior-lisp-program "/usr/bin/sbcl"))
 
 (require 'doom-themes)
 ;; Global settings (defaults)
@@ -12,7 +8,7 @@
 ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
 ;; may have their own settings.
 (load-theme 'doom-one t)
-(doom-themes-visual-bell-config)
+;;(doom-themes-visual-bell-config)
 
 ;; Enable custom neotree theme (all-the-icons must be installed!)
 (doom-themes-neotree-config)
@@ -21,3 +17,47 @@
 
 ;; Corrects (and improves) org-mode's native fontification.
 (doom-themes-org-config)
+
+(progn
+  (when (file-exists-p "/usr/local/bin/sbcl")
+    (setq inferior-lisp-program "/usr/local/bin/sbcl"))
+  (when (file-exists-p "/usr/bin/sbcl")
+    (setq inferior-lisp-program "/usr/bin/sbcl"))
+)
+
+(progn
+  (set-language-environment "UTF-8")
+  (set-terminal-coding-system 'utf-8)
+  (set-keyboard-coding-system 'utf-8)
+  (set-buffer-file-coding-system 'utf-8)
+  (setq default-buffer-file-coding-system 'utf-8)
+  (set-default-coding-systems 'utf-8)
+  (prefer-coding-system 'utf-8))
+
+(progn
+  (setq-default fill-column 80)
+  (setq text-mode-hook 'turn-on-auto-fill)
+  (setq default-major-mode 'text-mode)
+  (auto-fill-mode)
+)
+
+(setq indent-tabs-mode nil)
+(setq default-tab-width 2)
+(setq tab-width 2)
+;;(setq indent-line-function 'insert-tab)
+(global-prettify-symbols-mode 1)
+
+(defun my-setup-indent (n)
+  ;; java/c/c++
+  (setq c-basic-offset n)
+  ;; web development
+  (setq coffee-tab-width n) ; coffeescript
+  (setq javascript-indent-level n) ; javascript-mode
+  (setq js-indent-level n) ; js-mode
+  (setq js2-basic-offset n) ; js2-mode, in latest js2-mode, it's alias of js-indent-level
+  (setq web-mode-markup-indent-offset n) ; web-mode, html tag in html file
+  (setq web-mode-css-indent-offset n) ; web-mode, css in html file
+  (setq web-mode-code-indent-offset n) ; web-mode, js code in html file
+  (setq css-indent-offset n) ; css-mode
+  )
+(my-setup-indent 2)
--- emacs/doom.d/packages.el
+++ emacs/doom.d/packages.el
@@ -6,4 +6,5 @@
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
 (package! doom-themes)
+(package! xclip :disable t)
 ;;(package! doom-themes :recipe (:fetcher github :repo "hlissner/emacs-doom-themes"))
