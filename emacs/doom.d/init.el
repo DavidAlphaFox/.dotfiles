abfee8ffadbdc4bd72b29182deec6822fc0c2ca6
David.Gao <david.alpha.fox@gmail.com>
2020-02-01 23:04:52 +0800

    stop using racket

--- emacs/doom.d/init.el
+++ emacs/doom.d/init.el
@@ -139,7 +139,7 @@
        ;;purescript        ; javascript, but functional
        python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
-       racket            ; a DSL for DSLs
+       ;;racket            ; a DSL for DSLs
        ;;rest              ; Emacs as a REST client
        ;;ruby              ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
