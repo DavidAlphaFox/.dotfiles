8514b11d309cf951af54cf172192b729f95f2af3
David Gao <david.alpha.fox@gmail.com>
2020-09-02 22:30:13 +0800

    web

--- emacs/doom.d/init.el
+++ emacs/doom.d/init.el
@@ -164,8 +164,8 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       ;;web               ; the tubes
-       ;;yaml              ; JSON, but readable
+       web               ; the tubes
+       yaml              ; JSON, but readable
 
        :email
        ;;(mu4e +gmail)
