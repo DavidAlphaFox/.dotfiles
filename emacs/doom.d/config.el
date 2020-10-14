874e62677c67519ac89120a15775f8cbac40b3a4
David Gao <david.alpha.fox@gmail.com>
2020-10-14 20:59:13 +0800

    update color

--- emacs/doom.d/config.el
+++ emacs/doom.d/config.el
@@ -104,46 +104,6 @@
 
 ;;(global-undo-fu-session-mode)
 ;;(setq desktop-save-mode t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#f2e5bc" "#9d0006" "#79740e" "#b57614" "#076678" "#b16286" "#427b58" "#282828"])
- '(custom-safe-themes
-   (quote
-    ("e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" "632694fd8a835e85bcc8b7bb5c1df1a0164689bc6009864faed38a9142b97057" "9b01a258b57067426cc3c8155330b0381ae0d8dd41d5345b5eddac69f40d409b" default)))
- '(fci-rule-color "#504945")
- '(jdee-db-active-breakpoint-face-colors (cons "#f0f0f0" "#a89984"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#f0f0f0" "#79740e"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#f0f0f0" "#928374"))
- '(objed-cursor-color "#9d0006")
- '(pdf-view-midnight-colors (cons "#282828" "#fbf1c7"))
- '(rustic-ansi-faces
-   ["#fbf1c7" "#9d0006" "#79740e" "#b57614" "#076678" "#b16286" "#427b58" "#282828"])
- '(vc-annotate-background "#fbf1c7")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#79740e")
-    (cons 40 "#8d7410")
-    (cons 60 "#a17512")
-    (cons 80 "#b57614")
-    (cons 100 "#b3620e")
-    (cons 120 "#b14e08")
-    (cons 140 "#af3a03")
-    (cons 160 "#af472e")
-    (cons 180 "#b0545a")
-    (cons 200 "#b16286")
-    (cons 220 "#aa415b")
-    (cons 240 "#a32030")
-    (cons 260 "#9d0006")
-    (cons 280 "#9a2021")
-    (cons 300 "#97413c")
-    (cons 320 "#946258")
-    (cons 340 "#504945")
-    (cons 360 "#504945")))
- '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -153,5 +113,5 @@
 
 ;;(if (display-graphic-p)
 ;;    (load-theme 'doom-nord-light t)
-;;    (load-theme 'doom-acario-light t))
-(load-theme 'doom-solarized-light t)
+;;    (load-theme 'doom-tomorrow-day t))
+(load-theme 'doom-tomorrow-day t)
