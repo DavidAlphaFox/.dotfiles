49a5c5eee2a853880d1c4eb3693f72b48a45803e
David.Gao <david.alpha.fox@gmail.com>
2021-08-24 10:59:09 +0800

    delete guile

--- emacs/doom.d/init.el
+++ emacs/doom.d/init.el
@@ -159,7 +159,7 @@
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
-      (scheme +chez +guile +racket :checkers syntax) ; a fully conniving family of lisps
+      (scheme +chez +racket :checkers syntax) ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
