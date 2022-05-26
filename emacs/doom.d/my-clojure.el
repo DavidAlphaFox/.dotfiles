c1ec0633397c8c7df93760138dc0ffa89da8b342
David Gao <david.alpha.fox@gmail.com>
2022-05-26 21:00:48 +0800

    upgrade emacs

--- emacs/doom.d/my-clojure.el
+++ emacs/doom.d/my-clojure.el
@@ -4,10 +4,10 @@
   (add-hook 'clojurescript-mode-hook 'lsp)
   (add-hook 'clojurec-mode-hook 'lsp)
   (setq
-    gc-cons-threshold (* 100 1024 1024)
-    read-process-output-max (* 1024 1024)
-    company-idle-delay 0.0
-    company-minimum-prefix-length 1
+    ;;gc-cons-threshold (* 100 1024 1024)
+    ;;read-process-output-max (* 1024 1024)
+    ;;company-idle-delay 1.0
+    ;;company-minimum-prefix-length 2
     lsp-lens-enable t
     lsp-signature-auto-activate nil
      ;lsp-enable-indentation nil ; uncomment to use cider indentation instead of lsp
