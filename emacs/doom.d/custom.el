41ce54dc96916c266433399ddc5e4a02bc0ef8e1
David Gao <david.alpha.fox@gmail.com>
2022-06-22 20:44:12 +0800

    fix

--- emacs/doom.d/custom.el
+++ emacs/doom.d/custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(delete-selection-mode nil)
  '(ignored-local-variable-values '((eval setq is-dev t) (eval message "启动项目..."))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
