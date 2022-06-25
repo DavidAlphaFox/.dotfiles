95f5ec72ca0db0141d46589733e84261a24d1586
David Gao <david.alpha.fox@gmail.com>
2022-06-25 09:41:11 +0800

    detect the clojure and java

--- emacs/doom.d/init.el
+++ emacs/doom.d/init.el
@@ -115,7 +115,7 @@
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
        (cc +lsp)         ; C > C++ == 1
-       (:if (or IS-MAC IS-LINUX) (clojure +lsp))           ; java with a lisp
+       (:if HAS-CLOJURE (clojure +lsp))           ; java with a lisp
        common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
@@ -140,7 +140,7 @@
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        json              ; At least it ain't XML
-       (:if (or IS-MAC IS-LINUX) (java +lsp)); the poster child for carpal tunnel syndrome
+       (:if HAS-JAVA (java +lsp)); the poster child for carpal tunnel syndrome
        (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
--- emacs/doom.d/my-init.el
+++ emacs/doom.d/my-init.el
@@ -23,3 +23,21 @@
             (the-dotnet
               (my-utils/find-files dotnet-files)))
       (not (null the-dotnet)))))
+
+(defconst HAS-CLOJURE
+  (cond
+    ((or IS-MAC IS-LINUX) t)
+    (t (let* ((clojure-files
+             (my-utils/build-paths MY-BINARY-PATH '("clojure")))
+            (the-clojure
+              (my-utils/find-files clojure-files)))
+      (not (null the-clojure))))))
+
+(defconst HAS-JAVA
+  (cond
+    ((or IS-MAC IS-LINUX) t)
+    (t (let* ((java-files
+             (my-utils/build-paths MY-BINARY-PATH '("java")))
+            (the-java
+              (my-utils/find-files java-files)))
+      (not (null the-java))))))
