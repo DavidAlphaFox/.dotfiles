93f750426aa73ff8672c5408f74437a07bea6ef5
David Gao <david.alpha.fox@gmail.com>
2022-12-16 10:49:17 +0800

    delete chez

--- emacs/doom.d/init.el
+++ emacs/doom.d/init.el
@@ -164,7 +164,7 @@
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        (rust +lsp)              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
-       (scheme +racket +chez :checkers syntax)   ; a fully conniving family of lisps
+       (scheme +racket :checkers syntax)   ; a fully conniving family of lisps
        (racket +xp  :checkers syntax)            ; a DSL for DSLs
        sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
