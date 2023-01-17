ea89eaec3b6031e32d22e70d1e159e4933f48b82
David Gao <david.alpha.fox@gmail.com>
2023-01-17 11:54:37 +0800

    fix the emacs config

--- emacs/doom.d/my-init.el
+++ emacs/doom.d/my-init.el
@@ -17,14 +17,14 @@
 
 (defconst my/HAS-CLOJURE
   (cond
-    ((or my/IS-MAC my/IS-LINUX) t)
+    ((or IS-MAC IS-LINUX) t)
     (t (let* ((clojure-files (my/build-paths my/BINARY-PATH '("clojure")))
               (the-clojure (my/find-files clojure-files)))
          (not (null the-clojure))))))
 
 (defconst my/HAS-JAVA
   (cond
-   ((or my/IS-MAC my/IS-LINUX) t)
+   ((or IS-MAC IS-LINUX) t)
    (t (let* ((java-files (my/build-paths my/BINARY-PATH '("java")))
               (the-java (my/find-files java-files)))
         (not (null the-java))))))
