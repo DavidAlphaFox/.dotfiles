91685fd023867a49c7a604a182df84d4dd8439d3
David Gao <david.alpha.fox@gmail.com>
2023-10-22 10:45:03 +0800

    fix the lsp-keymap-prefix

--- emacs/spacemacs.d/my-lsp.el
+++ emacs/spacemacs.d/my-lsp.el
@@ -1,7 +1,7 @@
 ;;; my-lsp.el -*- lexical-binding: t; -*-
 
 ;;lsp
-(setq lsp-keymap-prefix "C-c C-l")
+(setq lsp-keymap-prefix "M-; l")
 (require 'lsp-mode)
 (setq lsp-ui-doc-position 'at-point)
 (setq lsp-ui-doc-show-with-mouse nil)
