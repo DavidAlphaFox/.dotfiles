f2c4d7540ed39de8a0d89553efdc2ef5663f7817
David Gao <david.alpha.fox@gmail.com>
2024-01-17 08:32:33 +0800

    change the theme

--- emacs/spacemacs.d/my-themes.el
+++ emacs/spacemacs.d/my-themes.el
@@ -1,5 +1,6 @@
-(defconst my/THEME
-  (if (display-graphic-p)
-    '(spacemacs-light)
-    '(spacemacs-dark)))
+;;(defconst my/THEME
+;;  (if (display-graphic-p)
+;;    '(spacemacs-light)
+;;    '(spacemacs-dark)))
+(defconst my/THEME '(spacemacs-dark))
 
