4bc68c7b3ab070810bd8382dfbf074d645b3f305
David Gao <david.alpha.fox@gmail.com>
2024-01-29 14:25:33 +0800

    enable inline hint of lsp

--- emacs/spacemacs.d/my-lsp.el
+++ emacs/spacemacs.d/my-lsp.el
@@ -3,6 +3,7 @@
 ;;lsp
 (setq lsp-keymap-prefix "M-; l")
 (require 'lsp-mode)
+(setq lsp-inlay-hint-enable t)
 (setq lsp-ui-doc-position 'at-point)
 (setq lsp-ui-doc-show-with-mouse nil)
 (setq lsp-groovy-server-file "~/.emacs.d/.cache/lsp/groovy/groovy-language-server-all.jar")
