9f6ac04cb53a394c914a40d26ade464b8f5b3db8
David Gao <david.alpha.fox@gmail.com>
2024-02-04 21:05:46 +0800

    lsp show ui

--- emacs/spacemacs.d/my-lsp.el
+++ emacs/spacemacs.d/my-lsp.el
@@ -5,5 +5,6 @@
 (require 'lsp-mode)
 (setq lsp-inlay-hint-enable t)
 (setq lsp-ui-doc-position 'at-point)
-(setq lsp-ui-doc-show-with-mouse nil)
+(setq lsp-ui-doc-enable t)
+(setq lsp-ui-doc-show-with-mouse t)
 (setq lsp-groovy-server-file "~/.emacs.d/.cache/lsp/groovy/groovy-language-server-all.jar")
