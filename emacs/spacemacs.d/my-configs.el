f464d2ffc5ed5fa3491454b714bdbbf24e810cb7
David Gao <david.alpha.fox@gmail.com>
2024-08-28 15:48:55 +0800

    fix the helm

--- emacs/spacemacs.d/my-configs.el
+++ emacs/spacemacs.d/my-configs.el
@@ -29,4 +29,4 @@
 ;; undo tree
 (with-eval-after-load 'undo-tree
   (setq undo-tree-auto-save-history nil))
-(defvar helm-ff-edit-marked-files-fn #'helm-marked-files-in-dired)
+;;(defvar helm-ff-edit-marked-files-fn #'helm-marked-files-in-dired)
