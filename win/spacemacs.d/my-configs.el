befa4e984a6241d0e833c58d6c55e2c737ca360b
David.Gao <david.alpha.fox@gmail.com>
2024-12-13 17:09:11 +0800

    Emacs using LF instead of CRLF on windows

--- win/spacemacs.d/my-configs.el
+++ win/spacemacs.d/my-configs.el
@@ -1,15 +1,16 @@
-(set-default-coding-systems 'utf-8)
-(prefer-coding-system 'utf-8)
-(setq default-buffer-file-coding-system 'utf-8)
-(set-buffer-file-coding-system 'utf-8)
-(set-language-environment "UTF-8")
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
+(setq default-coding-systems 'utf-8-unix)
+(prefer-coding-system 'utf-8-unix)
+(setq default-buffer-file-coding-system 'utf-8-unix)
+(setq buffer-file-coding-system 'utf-8-unix)
+(setq language-environment "UTF-8")
+(setq terminal-coding-system 'utf-8-unix)
+(setq keyboard-coding-system 'utf-8-unix)
+(setq coding-system-for-write 'utf-8-unix)
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
 (setq user-full-name "David Gao"
-  user-mail-address "david.alpha.fox@gmail.com")
+      user-mail-address "david.alpha.fox@gmail.com")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
