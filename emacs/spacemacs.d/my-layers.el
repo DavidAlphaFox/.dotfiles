41835dcd4099107dd73af5c247428303c3303d3e
David Gao <david.alpha.fox@gmail.com>
2025-05-06 20:28:02 +0800

    update layers

--- emacs/spacemacs.d/my-layers.el
+++ emacs/spacemacs.d/my-layers.el
@@ -41,7 +41,7 @@
        cmake-enable-cmake-ide-support t)))
 
 (defconst my/COMMON-DEVEL-LAYERS
-  '(yaml sql erlang
+  '(yaml sql erlang ocaml
 	 (shell :variables shell-default-height 30 shell-default-position 'bottom)
          (ruby :variables ruby-backend 'lsp)))
 
